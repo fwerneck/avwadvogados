beadc13e4f7dafc37fd860cb590f69fa5ad16957
Fernando Werneck <fewerneck21@gmail.com>
2022-02-17 09:19:51 -0300

    tirar o tel

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
 
     return(
               <footer className="fixed font-light bottom-0 w-full text-center bg-teal-800 text-gray-200 justify-center">
-                     Tel +55 (11) 3284 1163 | avw@avwadvogados.com.br
+                     +55 (11) 3284 1163 | avw@avwadvogados.com.br
               </footer>
 
         );
